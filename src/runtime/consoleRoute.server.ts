87c6dd911b3b7eeda2343a34ea5113ca949e18b6
clarkhu <huyujienice@gmail.com>
2023-08-29 11:04:42 +0800

    feat: add reqUrl logic

--- src/runtime/consoleRoute.server.ts
+++ src/runtime/consoleRoute.server.ts
@@ -3,7 +3,8 @@ import type { NitroAppPlugin } from "nitropack";
 export default <NitroAppPlugin>function (nitroApp) {
   // todo
   nitroApp.hooks.hook("render:html", (html, { event }) => {
-    console.log(`Accessed ${event.node.req.url}`);
+    const reqUrl = event.node.req?.url;
+    if (reqUrl && !reqUrl.includes("_nuxt")) console.log(`Accessed ${reqUrl}`);
   });
   // nitroApp.hooks.hook("render:response", (response, { event }) => {
   //   console.log("render:response");
